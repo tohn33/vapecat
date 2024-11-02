893e3ad898858f7eccd302a8d428a726e8e51978
tohn33 <web3john8@gmail.com>
2024-11-02 13:20:24 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*","./index.html",],
+  theme: {
+    extend: {
+       fontFamily: {
+      "Syncopate": ['Syncopate']  ,"Orbitron": ['Orbitron']
+  }},
+  },
+  plugins: [],
+}
+
